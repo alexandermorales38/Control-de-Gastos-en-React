67019c90ed317143a4a7e173cb3bba7626a04ae4
Alexander Dev <alexander@alexander.com>
2022-11-11 21:59:10 -0600

    Trabajando Formulario

--- src/Components/NuevoPresupuesto.jsx
+++ src/Components/NuevoPresupuesto.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
             return (
 
                 <div className='contenedor-presupuesto contenedor sombra'>
@@ -15,6 +15,8 @@ const NuevoPresupuesto = () => {
                                 className='nuevo-presupuesto'
                                 type="text"
                                 placeholder="Añade tu presupuesto"
+                                value={presupuesto}
+                                onChange={(e) => setPresupuesto(e.target.value)}
 
                             />
                         </div>
