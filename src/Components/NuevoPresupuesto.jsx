2827bba0b4a994b27d422edbecdd196823b98b61
Alexander Dev <alexander@alexander.com>
2022-12-12 19:05:57 -0600

    Trabajando Componente de Control

--- src/Components/NuevoPresupuesto.jsx
+++ src/Components/NuevoPresupuesto.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto, setIsValidPresupuesto}) => {
 
     const [mensaje, setMensaje] = useState ('')
 
@@ -9,12 +9,16 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
         const handlePresupuesto = (e) => {
             e.preventDefault();
 
-            if (!Number (presupuesto) || Number(presupuesto) < 0){
+            if (!presupuesto || presupuesto < 0)
+            
+            {
                 setMensaje('No es un presupuesto valido')
 
-              }  else {
-                    console.log('Si es un presupuesto valido')
-                }
+                return
+
+              } 
+                    setMensaje("")
+                    setIsValidPresupuesto(true)
 
         }
 
@@ -30,10 +34,10 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
 
                             <input
                                 className='nuevo-presupuesto'
-                                type="text"
+                                type="number"
                                 placeholder="Añade tu presupuesto"
                                 value={presupuesto}
-                                onChange={(e) => setPresupuesto(e.target.value)}
+                                onChange={(e) => setPresupuesto(Number(e.target.value))}
 
                             />
                         </div>
